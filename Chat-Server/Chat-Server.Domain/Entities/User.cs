74fcb99bb5c0a4bb4060458cae561d509ff31d83
Kirill <tpumoh>
2023-04-22 16:32:21 +0300

    Create DB,add table"Users"

---
+++ Chat-Server/Chat-Server.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_Server.Domain.Entities
+{
+	public  class User
+	{
+		public int Id { get; set; }
+		public string Login { get; set; }
+		public string Name { set; get; }
+		public string PasswordHash { get; set; }
+	}
+}
--- Chat-Server/Chat-Server/ViewModel/MainViewModel.cs
+++ Chat-Server/Chat-Server/ViewModel/MainViewModel.cs
@@ -1,11 +1,22 @@
+using Chat_Server.Context;
+using Chat_Server.Domain.Entities;
 using GalaSoft.MvvmLight;
 
 namespace Chat_Server.ViewModel
 {
 	public class MainViewModel : ViewModelBase
 	{
+		private ChatDBContext _context;
 		public MainViewModel()
 		{
+			_context = new ChatDBContext();
+			var user1 = new User() { Id = 1, Login = "2", Name = "3", PasswordHash = "4" };
+			_context.Users.Add(user1);
+			_context.SaveChanges();
+
+			//var user = new User() { Login = null, Name = "3", PasswordHash = "4" };
+			//_context.Users.Add(user);
+			//_context.SaveChanges();
 		}
 	}
 }
