cc172729391852a91a183abd81d0c838fa1a3b0b
Kirill <tpumoh>
2023-04-22 16:41:07 +0300

    Migration Is On

--- Chat-Server/Chat-Server/ViewModel/MainViewModel.cs
+++ Chat-Server/Chat-Server/ViewModel/MainViewModel.cs
@@ -9,14 +9,7 @@ namespace Chat_Server.ViewModel
 		private ChatDBContext _context;
 		public MainViewModel()
 		{
-			_context = new ChatDBContext();
-			var user1 = new User() { Id = 1, Login = "2", Name = "3", PasswordHash = "4" };
-			_context.Users.Add(user1);
-			_context.SaveChanges();
-
-			//var user = new User() { Login = null, Name = "3", PasswordHash = "4" };
-			//_context.Users.Add(user);
-			//_context.SaveChanges();
+			
 		}
 	}
 }
