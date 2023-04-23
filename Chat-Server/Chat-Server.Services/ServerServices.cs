f6e19d85d291ec069bb77e2dd2e96d9bf63177aa
Kirill <tpumoh>
2023-04-23 18:56:07 +0300

    Add RegistrationsUser,update CreateUserCommand

--- Chat-Server/Chat-Server.Services/ServerServices.cs
+++ Chat-Server/Chat-Server.Services/ServerServices.cs
@@ -1,5 +1,7 @@
 ﻿using Chat_Server.Context;
 using Chat_Server.Domain.Entities;
+using Chat_Server.Helpers;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Chat_Server.Services
@@ -8,9 +10,23 @@ namespace Chat_Server.Services
 	{
 		private ChatDBContext dBContext = new ChatDBContext();
 		
-		public async Task CreateUserAsync(User user)
+		public async Task<bool> CreateUserAsync(string log, string pass, string name)
 		{
-			var currentsUsers = await dBContext.Users.Select()
+			if(await Task.Run(() => dBContext.Users.Any(u => u.Login == log)) ||
+				await Task.Run(() => dBContext.Users.Any(u => u.Name == name)))
+			{
+				return false;
+			}
+
+			var user = new User 
+			{
+				Login = log,
+				PasswordHash = PasswordEncryptionHelper
+			}
+
+			 dBContext.Users.Add(user);
+			await dBContext.SaveChangesAsync();
+			return true;
 		}
 	}
 }
