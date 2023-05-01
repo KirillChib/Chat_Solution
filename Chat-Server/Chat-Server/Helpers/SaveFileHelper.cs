c1ec367b3eff527f0ca0d3cbb3c3678b0e71acdf
Kirill <tpumoh>
2023-05-01 16:23:08 +0300

    Add SaveFileHelper

---
+++ Chat-Server/Chat-Server/Helpers/SaveFileHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_Server.Helpers
+{
+	public static class SaveFileHelper
+	{
+		public static string SaveFile(string path,string fileName, byte[] file)
+		{
+			var fullPath = Path.Combine(path, fileName);
+			File.WriteAllBytes(fullPath, file);
+
+			return fullPath;
+		}
+	}
+}
