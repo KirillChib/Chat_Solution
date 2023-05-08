b527389d60bac392d4102fe4cc14b0d3bfe2d1b3
Kirill <tpumoh>
2023-05-08 12:29:11 +0300

    Add CreateChannelAsync

---
+++ Chat-Server/Chat-Server.Services/Channels/ChannelServices.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Chat_Server.Context;
+using Chat_Server.Domain.Entities;
+
+namespace Chat_Server.Services.Channels;
+
+public class ChannelServices : IChannelServices {
+	public async Task CreateChannelAsync(Channel channel) {
+		using var chatContext = new ChatDbContext();
+		chatContext.Channels.Add(channel);
+
+		await chatContext.SaveChangesAsync().ConfigureAwait(false);
+	}
+}
---
+++ Chat-Server/Chat-Server.Services/Channels/IChannelServices.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+using Chat_Server.Domain.Entities;
+
+namespace Chat_Server.Services.Channels; 
+
+public interface IChannelServices {
+	Task CreateChannelAsync(Channel channel);
+}
