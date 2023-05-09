17f5473f292471110b9b5aafaf04332a6048bd9e
Kirill <tpumoh>
2023-05-09 19:42:48 +0300

    Add BlockingServices

---
+++ Chat-Server/Chat-Server.Services/Blockings/BlockingServices.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Chat_Server.Context;
+using Chat_Server.Domain.Entities;
+
+namespace Chat_Server.Services.Blockings; 
+
+public class BlockingServices : IBlockingServices {
+	public async Task CreateBlockingAsync(Blocking blocking) {
+		using var chatContext = new ChatDbContext();
+		chatContext.Blockings.Add(blocking);
+
+		await chatContext.SaveChangesAsync().ConfigureAwait(false);
+	}
+	public async Task<ICollection<Blocking>> GetBlockingsByUserIdAsync(int userId) {
+		using var chatContext = new ChatDbContext();
+		return await chatContext.Blockings.Where(b => b.UserId == userId).ToListAsync().ConfigureAwait(false);
+	}
+	public async Task<bool> BlockingExistAsync(Blocking blocking) {
+		using var chatContext = new ChatDbContext();
+		return await chatContext.Blockings.AnyAsync(b => b.UserId == blocking.UserId && b.BlockingUserId == blocking.BlockingUserId).ConfigureAwait(false);
+	}
+	public async Task DeleteBlockingAsync(Blocking blocking) {
+		using var chatContext = new ChatDbContext();
+		chatContext.Blockings.Attach(blocking);
+		chatContext.Blockings.Remove(blocking);
+
+		await chatContext.SaveChangesAsync().ConfigureAwait(false);
+	}
+}
