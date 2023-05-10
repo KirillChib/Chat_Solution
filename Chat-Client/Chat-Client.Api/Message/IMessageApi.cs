eeb94f81018c54590c38c8f4ae4f12cdb2a45228
Kirill <tpumoh>
2023-05-10 19:39:50 +0300

    Add GetUserMessagesRequestAsync

--- Chat-Client/Chat-Client.Api/Message/IMessageApi.cs
+++ Chat-Client/Chat-Client.Api/Message/IMessageApi.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Chat_Client.Api.Request;
 
 namespace Chat_Client.Api.Message;
 
 public interface IMessageApi {
 	Task SendMessageToUserRequestAsync(string token, MessageToUser message);
+	Task<ICollection<MessageToUser>> GetUserMessagesRequestAsync(string token, int userId);
 }
--- Chat-Client/Chat-Client.Api/Message/MessageApi.cs
+++ Chat-Client/Chat-Client.Api/Message/MessageApi.cs
@@ -1,13 +1,17 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Chat_Client.Api.Request;
 
-namespace Chat_Client.Api.Message; 
+namespace Chat_Client.Api.Message;
 
-public class MessageApi : ApiBase, IMessageApi{
+public class MessageApi : ApiBase, IMessageApi {
 	public MessageApi(string baseUri) : base(baseUri) {
 	}
 	public async Task SendMessageToUserRequestAsync(string token, MessageToUser message) {
 		await SendAsync(HttpMethod.Get, @"/messages", token, message);
+	}
+	public async Task<ICollection<MessageToUser>> GetUserMessagesRequestAsync(string token, int userId) {
+		return await SendAsync<ICollection<MessageToUser>>(HttpMethod.Get, $@"/messages/{userId}", token).ConfigureAwait(false);
 	}
 }
