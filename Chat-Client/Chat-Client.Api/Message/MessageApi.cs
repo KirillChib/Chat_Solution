bf53923497a854e68ca6f385a05aeabcc19cb668
Kirill <tpumoh>
2023-05-11 18:46:07 +0300

    Add GetNewUserMessageRequestAsync

--- Chat-Client/Chat-Client.Api/Message/MessageApi.cs
+++ Chat-Client/Chat-Client.Api/Message/MessageApi.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Chat_Client.Api.Request;
+using Chat_Client.Api.Response;
 
 namespace Chat_Client.Api.Message;
 
@@ -13,5 +14,8 @@ public class MessageApi : ApiBase, IMessageApi {
 	}
 	public async Task<ICollection<MessageToUser>> GetUserMessagesRequestAsync(string token, int userId) {
 		return await SendAsync<ICollection<MessageToUser>>(HttpMethod.Get, $@"/messages/{userId}", token).ConfigureAwait(false);
+	}
+	public async Task<ICollection<MessageFromUser>> GetNewUserMessageRequestAsync(string token, int userId) {
+		return await SendAsync<ICollection<MessageFromUser>>(HttpMethod.Get, $@"/NewMessages/{userId}", token).ConfigureAwait(false);
 	}
 }
