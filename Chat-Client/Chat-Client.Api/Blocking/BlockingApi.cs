09ba65ac4e9810dd20999755826e393ca46ec1ee
Kirill <tpumoh>
2023-05-11 19:50:29 +0300

    Add BlockingApi

---
+++ Chat-Client/Chat-Client.Api/Blocking/BlockingApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Chat_Client.Api.Request;
+
+namespace Chat_Client.Api.Blocking; 
+
+public class BlockingApi : ApiBase, IBlockingApi {
+	public BlockingApi(string baseUri) : base(baseUri) {
+	}
+	public async Task<string> CreateBlockingRequestsAsync(BlockingRequest blocking, string token) {
+		var response = await SendAsync(HttpMethod.Post, @"/blocking", token, blocking).ConfigureAwait(false);
+		return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+	}
+}
---
+++ Chat-Client/Chat-Client.Api/Blocking/IBlockingApi.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+using Chat_Client.Api.Request;
+
+namespace Chat_Client.Api.Blocking; 
+
+public interface IBlockingApi {
+	Task<string> CreateBlockingRequestsAsync(BlockingRequest blocking, string token);
+}
