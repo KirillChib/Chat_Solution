1bd006cff916588a1254055d2bfb5cdc468cbc52
Kirill <tpumoh>
2023-05-12 16:52:36 +0300

    Add SubscribeChannelRequestAsync

--- Chat-Client/Chat-Client.Api/Channel/ChannelApi.cs
+++ Chat-Client/Chat-Client.Api/Channel/ChannelApi.cs
@@ -10,4 +10,8 @@ public class ChannelApi : ApiBase, IChannelApi{
 		var response = await SendAsync(HttpMethod.Post, @"/channels", token).ConfigureAwait(false);
 		return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
 	}
+	public async Task<string> SubscribeChannelRequestAsync(string token, int channelId) {
+		var response = await SendAsync(HttpMethod.Post, $@"/channels/{channelId}", token).ConfigureAwait(false);
+		return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+	}
 }
--- Chat-Client/Chat-Client.Api/Channel/IChannelApi.cs
+++ Chat-Client/Chat-Client.Api/Channel/IChannelApi.cs
@@ -4,4 +4,5 @@ namespace Chat_Client.Api.Channel;
 
 public interface IChannelApi {
 	Task<string> CreateChannelRequestAsync(string token, string channelName);
+	Task<string> SubscribeChannelRequestAsync(string token, int channelId);
 }
