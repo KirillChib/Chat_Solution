8d5ad9dc5985c5fd3e1bc58a7920bda0961bad44
Kirill <tpumoh>
2023-05-12 16:45:30 +0300

    Add ChannelApi

---
+++ Chat-Client/Chat-Client.Api/Channel/IChannelApi.cs
@@ -0,0 +1,7 @@
+﻿using System.Threading.Tasks;
+
+namespace Chat_Client.Api.Channel; 
+
+public interface IChannelApi {
+	Task<string> CreateChannelRequestAsync(string token, string channelName);
+}
